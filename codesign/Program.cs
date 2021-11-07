de20963c1e81a4d349e8bd359bb38a2dcc464b96
Filip Navara <navara@emclient.com>
2021-11-07 23:19:11 +0100

    Add very simple command line utility

--- codesign/Program.cs
+++ codesign/Program.cs
@@ -1,33 +1,104 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using Azure.Core;
+using Azure.Identity;
+using Azure.Security.KeyVault.Certificates;
+using Claunia.PropertyList;
+using System.CommandLine;
+using System.CommandLine.Binding;
+using System.CommandLine.Invocation;
+using System.CommandLine.IO;
+using System.CommandLine.Parsing;
+using System.Security.Cryptography.X509Certificates;
 using Melanzana.CodeSign;
+using RSAKeyVaultProvider;
 
-//var provisioningProfile = new ProvisioningProfile("/Users/filipnavara/Library/MobileDevice/Provisioning Profiles/fd169b8e-5183-4fa1-af17-e9086a2adbcc.mobileprovision");
-//var certificate = store.Certificates.Find(X509FindType.FindByIssuerName)
-X509Certificate2? developerCertificate = null;
-// Find certificate with private key
-var store = new X509Store(StoreLocation.CurrentUser);
-store.Open(OpenFlags.ReadOnly);
-/*foreach (var certificate in provisioningProfile.DeveloperCertificates)
+namespace Melanzana.CodeSign
 {
-    developerCertificate = store.Certificates.Find(X509FindType.FindBySubjectName, certificate.SubjectName, true).FirstOrDefault();
-    if (developerCertificate != null)
-        break;
-}*/
-/*developerCertificate = store.Certificates.Find(
-    X509FindType.FindByThumbprint, 
-    "D69F66D03D0DD5E7B2270300E60844898B70A213",
-    //new byte[] { 0xD6, 0x9F, 0x66, 0xD0, 0x3D, 0x0D, 0xD5, 0xE7, 0xB2, 0x27, 0x03, 0x00, 0xE6, 0x08, 0x44, 0x89, 0x8B, 0x70, 0xA2, 0x13 },
-    true).First();
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            var signCommand = new Command("sign", "Sign code at path using given identity")
+            {
+                new Argument<string>("identity", "Name of signing identity or \"-\" for ad-hoc signing"),
+                new Argument<string>("path", "Path to bundle or executable on disk"),
+                new Option<string?>("--entitlements", "Path to entitlements to embed into the signature"),
+                new Option<string?>("--azure-key-vault-url", "URL to an Azure Key Vault"),
+            };
 
-Debug.Assert(developerCertificate != null);*/
+            signCommand.Handler = CommandHandler.Create<string, string, string?, string?>(HandleSign);
 
-var signer = new Signer(new CodeSignOptions {
-    DeveloperCertificate = developerCertificate,
-});
-    //developerCertificate, null /*new Entitlements(provisioningProfile.Entitlements)*/);
-//var stopwatch = new Stopwatch();
-//stopwatch.Start();
-signer.Sign("/Users/filipnavara/Projects/codesign/Airmail.app");
-    ///*"/Applications/Airmail.localized/Airmail.app"*/ "/Users/filipnavara/Downloads/gc/MailClient.Mobile.iOS.app/");
-//stopwatch.Stop();
-//Console.WriteLine("Stops: " + stopwatch.Elapsed);
+            return new RootCommand { signCommand }.Invoke(args);
+        }
+
+        private static X509Certificate2? FindCertificate(string identity)
+        {
+            if (identity == "-")
+            {
+                // Ad-hoc signing
+                return null;
+            }
+
+            var store = new X509Store(StoreLocation.CurrentUser);
+            store.Open(OpenFlags.ReadOnly);
+            X509Certificate2Collection collection;
+
+            if (identity.Length == 40 && identity.All(Uri.IsHexDigit))
+            {
+                collection = store.Certificates.Find(
+                    X509FindType.FindByThumbprint, 
+                    identity,
+                    true);
+            }
+            else
+            {
+                collection = store.Certificates.Find(
+                    X509FindType.FindBySubjectName,
+                    identity,
+                    true);
+            }
+
+            var certificate = collection.Where(c => c.IsAppleDeveloperCertificate()).FirstOrDefault();
+            if (certificate == null)
+            {
+                throw new Exception($"Certificate for identity \"{identity}\" not found");
+            }
+
+            return certificate;
+        }
+
+        public static void HandleSign(
+            string identity,
+            string path,
+            string? entitlements,
+            string? azureKeyVaultUrl)
+        {
+            var codeSignOptions = new CodeSignOptions();
+
+            if (String.IsNullOrEmpty(azureKeyVaultUrl))
+            {
+                codeSignOptions.DeveloperCertificate = FindCertificate(identity);
+            }
+            else
+            {
+                // Azure key vault
+                var credential = new AzureCliCredential();
+                var certClient = new CertificateClient(new Uri(azureKeyVaultUrl), credential);
+                var azureCertificate = certClient.GetCertificate(identity).Value;
+                codeSignOptions.DeveloperCertificate = new X509Certificate2(azureCertificate.Cer);
+                codeSignOptions.PrivateKey = RSAFactory.Create(credential, azureCertificate.KeyId, codeSignOptions.DeveloperCertificate);
+            }
+
+            if (!String.IsNullOrEmpty(entitlements))
+            {
+                codeSignOptions.Entitlements = new Entitlements(
+                    (NSDictionary)PropertyListParser.Parse(
+                        new FileInfo(entitlements)
+                    )
+                );
+            }
+
+            var signer = new Signer(codeSignOptions);
+            signer.Sign(path);
+        }
+    }
+}

a55957cf39ea5edc2575c3f103693c67ae8ce594
Filip Navara <navara@emclient.com>
2021-11-07 19:57:35 +0100

    Add skeleton of FAT roundtrip test

--- Melanzana.MachO.Tests/RoundtripTests.cs
+++ Melanzana.MachO.Tests/RoundtripTests.cs
@@ -7,13 +7,11 @@ namespace Melanzana.MachO.Tests
 {
     public class RoundtripTests
     {
-        [Fact]
-        public void BasicRoundtrip()
+        private static void TestRoundtrip(Stream aOutStream)
         {
-            var aOutStream = typeof(RoundtripTests).Assembly.GetManifestResourceStream("Melanzana.MachO.Tests.Data.a.out")!;
             var outputStream = new MemoryStream();
-            var objectFile = MachReader.Read(aOutStream).First();
-            MachWriter.Write(outputStream, objectFile);
+            var objectFiles = MachReader.Read(aOutStream).ToList();
+            MachWriter.Write(outputStream, objectFiles);
 
             aOutStream.Position = 0;
             outputStream.Position = 0;
@@ -24,6 +22,23 @@ namespace Melanzana.MachO.Tests
             aOutStream.ReadFully(input);
             outputStream.ReadFully(output);
             Assert.Equal(input, output);
+        }
+
+        [Fact]
+        public void BasicRoundtrip()
+        {
+            var aOutStream = typeof(RoundtripTests).Assembly.GetManifestResourceStream("Melanzana.MachO.Tests.Data.a.out")!;
+            TestRoundtrip(aOutStream);
+        }
+
+        //[Fact]
+        public void FatRoundtrip()
+        {
+            // FIXME: This doesn't work yet because the x86-64 object file pads the code in __TEXT section with
+            // 0x90 byte that we don't roundtrip since it's not part of the section. The executable still works
+            // after rewriting though.
+            var aFatOutStream = typeof(RoundtripTests).Assembly.GetManifestResourceStream("Melanzana.MachO.Tests.Data.a.fat.out")!;
+            TestRoundtrip(aFatOutStream);
         }
     }
 }
